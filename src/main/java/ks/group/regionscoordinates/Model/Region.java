4c6742a6e0711d95cbf0a9ced7a6dfe090359a28
Karolis <sadeckas.karolisx@gmail.com>
2023-06-28 22:48:05 +0200

    REMOVE: subRegionCoordinates list
    No batter solution was found using subRegionCoordinate list - still complicated aproach which requires 3 nested loops.

--- src/main/java/ks/group/regionscoordinates/Model/Region.java
+++ src/main/java/ks/group/regionscoordinates/Model/Region.java
@@ -1,14 +1,10 @@
 package ks.group.regionscoordinates.Model;
 
-import org.locationtech.jts.geom.Coordinate;
-
-import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Region {
     private String name;
     private double[][][] coordinates;
-    private ArrayList<Coordinate[]> subRegionCoordinates = new ArrayList<>();
 
     public Region(String name, double[][][] coordinates) {
         this.name = name;
@@ -21,23 +17,6 @@ public class Region {
 
     public double[][][] getCoordinates() {
         return coordinates;
-    }
-
-    // Inside Array we have "flat" list of the SUB-Region coordinates
-    public ArrayList<Coordinate[]> getSubRegionsCoordinates() {
-        subRegionCoordinates = new ArrayList<>();
-
-        for (int i = 0; i < coordinates.length; i++) {
-            Coordinate[] coordinatesToAdd = new Coordinate[coordinates[i].length];
-            for (int j = 0; j < coordinates[i].length; j++) {
-                double latitude = coordinates[i][j][0];
-                double longitude = coordinates[i][j][1];
-                Coordinate coordinate = new Coordinate(latitude, longitude);
-                coordinatesToAdd[j] = coordinate;
-            }
-            subRegionCoordinates.add(coordinatesToAdd);
-        }
-        return subRegionCoordinates;
     }
 
     @Override
